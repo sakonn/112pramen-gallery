fd768178cea1abf4baa0fed628ef3bc2d5d1c1e4
Jakub Korečko <jakub@korymont.eu>
2021-02-08 17:55:05 +0100

    Create flickrapi object and refactor code to be modular

--- albums.php
+++ albums.php
@@ -1,5 +1,6 @@
 <?php
   //include 'classes.php';
+	use Tracy\Debugger;
 ?>
 
 <div class="dialog text-center" style="display:none;">
@@ -11,26 +12,20 @@
 <div id="customMyGallery" >
 
 <?php
-  foreach ($x->photosets->photoset as $key=>$photoset) {
-		//Debugger::barDump($photoset);
-		$farm_id=$photoset->farm;
-		$server_id=$photoset->server;
-		$photo_id=$photoset->primary;
-		$secret=$photoset->secret;
-		$size='w';
-	
+  foreach ($api->geAlbumsList() as $key=>$album) {
+		
 		echo '
 		<div class="customCover">
 			<div class="customGal">
-        <a class="customAlb" href="?id='.$photoset->id.'">
-          <div id="'.$photoset->id.'" gallerynumber="'.$key.'" class="customImage" style="background-image: url(&quot;'.Photos::createPhotos($farm_id,$server_id,$photo_id,$secret,$size).'&quot;);"></div>
+        <a class="customAlb" href="?id='.$api->getAlbumID($album).'">
+          <div id="'.$api->getAlbumID($album).'" gallerynumber="'.$key.'" class="customImage" style="background-image: url(&quot;'.$api->getAlbumCoverImageURL($album).'&quot;);"></div>
         </a>
-        <a class="customLink" href="?id='.$photoset->id.'">
-          '.$photoset->title->_content.'
+        <a class="customLink" href="?id='.$api->getAlbumID($album).'">
+          '.$api->getAlbumTitle($album).'
         </a>
 				<div class="customInfo">
 					<span class="customSum">
-					'.$photoset->count_photos.' fotiek
+					'.$api->getAlbumPhotoCount($album).' fotiek
 					</span>
 					<span class="customShare">
 						Zdieľať
@@ -41,8 +36,6 @@
 		';
   }
   
-  // <div id="72157716208936413" gallerynumber="0" class="album" style="background-image: url(&quot;https://farm5.staticflickr.com//65535//50404830328_14dafbaee0_n.jpg&quot;);"><div class="galleryBackground"><div class="galleryInfo"><div class="galleryDesc"><a>Otvorenie skautského roka 2020/21</a></div><button class="shareButton plain-button" title="Zdieľať galériu" style="height: 0px;"><a class="shareGallery"></a></button></div></div></div>
-	
 ?>
 
 </div>
---
+++ config.example.php
@@ -0,0 +1,7 @@
+<?php
+$config = [
+  'flickr' => [
+    'key' => 'XXXXXXXXXXXX',
+    'user' => 'XXXXXXXXXXXX'
+  ]
+];
